bcf9cf4d10d535dbac9af0a6923d0a9e9403affa
basarevski.vitali <basarevskiy.v.v@gmail.com>
2021-07-30 18:13:54 +0300

    finished awesome bash script

--- script.sh
+++ script.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-ls -la /var/log/
+LOG_FILE=/var/log/syslog
+CURRENT_DATE=$(date +"%b %d")
+
+#echo $CURRENT_DATE
+
+if [ -e $LOG_FILE ]
+  then
+    echo "-------------------- First 10 lines --------------------"
+    cat $LOG_FILE | head -n 10
+    echo "-------------------- Last 10 lines --------------------"
+    cat $LOG_FILE | tail -n 10
+    echo "-------------------- First 10 lines as of $CURRENT_DATE  --------------------"
+    sed -n "/^$CURRENT_DATE/p" $LOG_FILE | head -n 10
+    echo "-------------------- All cron & anacron logs  --------------------"
+    cat $LOG_FILE | grep cron
+  else
+    echo "File $LOG_FILE doesn't exist"
+fi
