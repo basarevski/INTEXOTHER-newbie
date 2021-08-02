bb607981de7c40d8e1f0e9e255fcebd2ba3819f4
basarevski.vitali <basarevskiy.v.v@gmail.com>
2021-08-02 18:48:57 +0300

    finish bash script

--- script.sh
+++ script.sh
@@ -6,11 +6,13 @@ CURRENT_DATE=$(date +"%b %d")
 EXECUTION_TIME=$(date +"%H:%M:%S")
 OUTPUT_PATH=$PWD/output_logs
 
-if [ ! -d "$OUTPUT_PATH" ];
+# Check output folder
+if [ ! -d "$OUTPUT_PATH" ]
   then
     mkdir "$OUTPUT_PATH"
 fi
 
+# Create log files
 if [ -e $LOG_FILE ]
   then
     cat $LOG_FILE | head -n 10 > "$OUTPUT_PATH"/"${EXECUTION_TIME}_${LOG_FILE_NAME}_first-10"
@@ -20,3 +22,6 @@ if [ -e $LOG_FILE ]
   else
     echo "File $LOG_FILE doesn't exist"
 fi
+
+# Remove files older than 10 minutes
+find "$OUTPUT_PATH" -type f  -mmin +10 -delete
