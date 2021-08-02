75e4559df3cce2ad00206c035be3b34857f7b369
basarevski.vitali <basarevskiy.v.v@gmail.com>
2021-08-02 16:31:23 +0300

    finish bash script

--- script.sh
+++ script.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 LOG_FILE=/var/log/syslog
+LOG_FILE_NAME=syslog
+OUTPUT_PATH=/home/vitali/Documents/scriptOutput
 CURRENT_DATE=$(date +"%b %d")
-
-#echo $CURRENT_DATE
 
 if [ -e $LOG_FILE ]
   then
-    echo "-------------------- First 10 lines --------------------"
-    cat $LOG_FILE | head -n 10
-    echo "-------------------- Last 10 lines --------------------"
-    cat $LOG_FILE | tail -n 10
-    echo "-------------------- First 10 lines as of $CURRENT_DATE  --------------------"
-    sed -n "/^$CURRENT_DATE/p" $LOG_FILE | head -n 10
-    echo "-------------------- All cron & anacron logs  --------------------"
-    cat $LOG_FILE | grep cron
+    cat $LOG_FILE | head -n 10 > $OUTPUT_PATH/"${LOG_FILE_NAME}_first-10"
+    cat $LOG_FILE | tail -n 10 > $OUTPUT_PATH/"${LOG_FILE_NAME}_last-10"
+    awk -v date="$CURRENT_DATE" '$0 ~ date {p=1} p' $LOG_FILE| head -n 10 > $OUTPUT_PATH/"${LOG_FILE_NAME}_today-10"
+    cat $LOG_FILE | grep cron > $OUTPUT_PATH/"${LOG_FILE_NAME}_cron-full"
   else
     echo "File $LOG_FILE doesn't exist"
 fi
