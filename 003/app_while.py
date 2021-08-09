9dea89be9fcf9c764a8d6d8ca6cafbfb66df5e1d
basarevski.vitali <basarevskiy.v.v@gmail.com>
2021-08-09 13:11:33 +0300

    move other projects to branch

---
+++ 003/app_while.py
@@ -0,0 +1,52 @@
+def bubble_sort():
+    list_sort = []
+    numbers_input = input("Укажи через пробел числа для сортировки и нажми Enter когда закончишь: ")
+    list_input = numbers_input.split()
+
+    for item in list_input:
+        try:
+            list_sort.append(int(item))
+        except ValueError:
+            print("Не корректный формат данных: ", item, " Ожидается целое число.")
+
+    print('\n')
+    print("Не сортированный список", '\n', list_sort)
+
+    item = 0
+    while item < len(list_sort) - 1:
+        number = 0
+        while number < len(list_sort) - 1 - item:
+            if list_sort[number] > list_sort[number+1]:
+                list_sort[number], list_sort[number+1] = list_sort[number+1], list_sort[number]
+            number += 1
+        item += 1
+
+    print(list_sort)
+
+
+print(bubble_sort())
+
+
+# list_sort = []
+# numbers_input = input("Укажи через пробел числа для сортировки и нажми Enter когда закончишь: ")
+# list_input = numbers_input.split()
+#
+# for item in list_input:
+#     try:
+#         list_sort.append(int(item))
+#     except ValueError:
+#         print("Не корректный формат данных: ", item, " Ожидается целое число.")
+#
+# print('\n')
+# print("Не сортированный список", '\n', list_sort)
+#
+# item = 0
+# while item < len(list_sort) - 1:
+#     number = 0
+#     while number < len(list_sort) - 1 - item:
+#         if list_sort[number] > list_sort[number+1]:
+#             list_sort[number], list_sort[number+1] = list_sort[number+1], list_sort[number]
+#         number += 1
+#     item += 1
+#
+# print(list_sort)
---
+++ 004/lang.py
@@ -0,0 +1,8 @@
+lang = input("Лучший язык программирования: ")
+
+if lang == "Bash" or lang == "bash" or lang == "Баш" or lang == "баш":
+    print("Согласен. Bash - лучший язык программирования.")
+elif lang == "Python" or lang == "python" or lang == "Пайтон" or lang == "пайтон" or lang == "Питон" or lang == "питон":
+    print("Согласен. Python - лучший язык программирования.")
+else:
+    print("Не удалось распознать строку")
